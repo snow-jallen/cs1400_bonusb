0d43c2249f074a35499bd216e72e2aa00644014d
Benson Bird <benson.bird@students.snow.edu>
2023-10-25 08:35:03 -0600

    DoesNotCompile

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Console;
+
+namespace CompilerErrors
+{
+    int x = 23;
+
+    class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                Utilit.AdditionWihtoutOverflow(int.MaxValue, 25) //I made this method. It is in Utilities and I DO NOT want to move that method into my Program class
+            
+            catch(Exception e)
+            {
+                WriteLine(e.Message) //I want it to print: "Cannot Add that big of numbers because of overflow"
+            }
+
+            double sum = CompilerErrors.Utilit.AdditionWihtoutOverflow(num1, num2)
+
+            WriteLine($"The sum of {num1} and {num2} is {sum}") //It should print The sum of 4 and 5 is 9
+        }
+    }
+}
+
+
+
+
---
+++ Utilities.cs
@@ -0,0 +1,22 @@
+﻿namespace CompilerErrors
+{
+    int i = 4;
+
+    class Utilities
+    {
+        private int AdditionWihtoutOverflow(int num1, int num2)
+        {
+            double doubleSum = (double)num1 + num2;
+
+            if(doubleSum == num1 + num2)
+            {
+                return num1 + num2
+            }
+            else
+            {
+                Console.WriteLine("Cannot Add that big of numbers because of overflow")
+            }
+        }
+    }
+}
+
